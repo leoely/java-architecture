785ea15f4a9568dafc193a2fb35df2dfd2c517d5
Leo Brad <>
2024-07-14 17:41:31 +0800

    [Add] Add gateway.

--- kafka1/src/main/java/com/example/kafka1/Kafka1Application.java
+++ kafka1/src/main/java/com/example/kafka1/Kafka1Application.java
@@ -3,9 +3,11 @@ package com.example.kafka1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.kafka1.mapper")
+@EnableDiscoveryClient
 public class Kafka1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Kafka1Application.class, args);
