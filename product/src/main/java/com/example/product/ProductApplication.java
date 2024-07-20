1cb8320c6a54f0fe92b09c7ee182426b655ca2ae
Leo Brad <>
2024-07-20 12:12:15 +0800

    [Add] Add common module.

--- product/src/main/java/com/example/product/ProductApplication.java
+++ product/src/main/java/com/example/product/ProductApplication.java
@@ -3,9 +3,11 @@ package com.example.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.product.mapper")
+@EnableDiscoveryClient
 public class ProductApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
